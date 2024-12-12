615599e013dc81b985e934760f8491209c6fc15a
ZetoOfficial <tpptgs@bk.ru>
2024-12-12 20:18:07 +0500

    add cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import routes
 
@@ -6,6 +7,14 @@ app = FastAPI(
     title="Маршрутизация Кабинетов API",
     description="API для получения списка объектов и построения маршрутов между кабинетами.",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(routes.router)
